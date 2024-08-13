287d3a74ee1fc68b287a7b05b22521fb6f002407
Raya Mahardika <rayapkun@hlcyn.co>
2024-08-14 05:22:11 +0700

    Revert "tissot: Drop device manifest."
    This reverts commit 8aebf9117918211b755b371d6008f79401953472.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,9 @@ DEVICE_PATH := device/xiaomi/tissot
 # Display
 TARGET_SCREEN_DENSITY := 440
 
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+
 # Kernel
 TARGET_KERNEL_CONFIG += xiaomi/tissot.config
 
