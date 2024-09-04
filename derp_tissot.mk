47fe35d70379ad800ee1a428f9e6befa9774f195
Raya Mahardika <rayapkun@hlcyn.co>
2024-09-05 02:52:52 +0700

    Update derp_tissot.mk

--- derp_tissot.mk
+++ derp_tissot.mk
@@ -10,6 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Derpfest stuff.
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
+TARGET_USES_MINI_GAPPS := true
 
 # Inherit from tissot device
 TARGET_IS_AB := true
