c5552c4b9f0432b4d53b0d3881527f0e21b12cc1
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-19 02:44:05 +0200

    tissot: switch to common QCOM AIDL bootctrl HAL
    Change-Id: I04ab771d3b1c38b58913607fbff1bb3b55e1fe25

--- device.mk
+++ device.mk
@@ -29,13 +29,8 @@ PRODUCT_PACKAGES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl.recovery \
-    bootctrl.msm8953.recovery
-
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service \
-    bootctrl.msm8953 \
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
