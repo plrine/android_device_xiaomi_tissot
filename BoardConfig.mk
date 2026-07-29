6b4c97f35044165a934751500c7d00157943d246
Raya Mahardika <94042378+rktdnt@users.noreply.github.com>
2026-07-29 13:28:03 +0700

    tissot: Update defconfig for 4.19 kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,7 @@ BOARD_USES_RECOVERY_AS_BOOT := true
 TARGET_NO_RECOVERY := true
 
 # Kernel
-TARGET_KERNEL_CONFIG += xiaomi/tissot.config
+TARGET_KERNEL_CONFIG += vendor/tissot.config
 
 # A/B
 AB_OTA_PARTITIONS += \
